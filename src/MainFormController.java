b400a0ef56bff9a4310dfb8dbfaa67728709e37d
tasheela <tasheelajay1999@gmail.com>
2023-08-29 20:36:08 +0530

    Hibernate - student management system project created

---
+++ src/MainFormController.java
@@ -0,0 +1,19 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MainFormController {
+    public AnchorPane context;
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public Button btn;
+    public TableView tblStudent;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colDelete;
+    public TableColumn colUpdate;
+}
